2d8e96d863e36c0162ac7da7b808215ad7dd45ab
Mikhail-Ponomarev <misheleponomarev555@gmail.com>
2023-10-31 12:08:55 +0300

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+const elem = document.getElementById("root");
+const txt = "Hello world!";
+const style = "text";
+const clickInput = () => {
+  console.log("Click");
+};
+const input = (
+  <input placeholder={txt} className={style} onClick={clickInput} />
+);
+
+ReactDOM.render(input, elem);
